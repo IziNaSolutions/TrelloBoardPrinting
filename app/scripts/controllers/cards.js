e959113f4c8ee5c2952a91d877a5a835cb01eaeb
idanizi <idanizi@gmail.com>
2017-07-04 02:56:49 +0300

    generalization

--- app/scripts/controllers/cards.js
+++ app/scripts/controllers/cards.js
@@ -8,14 +8,17 @@
  * Controller of the trelloBoardPrintingApp
  */
 angular.module('trelloBoardPrintingApp')
-    .controller('CardsCtrl', function(cards) {
+    .controller('CardsCtrl', function(cards, $rootScope, $log) {
 
         //////////////// init ///////////////
 
         var self = this;
-        cards.getCards()
+        cards.getCards($rootScope.url, $rootScope.cookie)
             .then(function(res) {
                 self.data = res;
+                $rootScope.title = res.name;
+                window.title = $rootScope.title;
+                $log.info('window title', window.title);
             });
 
 
